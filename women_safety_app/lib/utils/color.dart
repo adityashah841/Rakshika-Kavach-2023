8f6e15f4c434a7468b76de793f8b0c0b76c8d5e3
Krish <krishshah235@gmail.com>
2023-07-15 12:48:53 +0530

    bottom bar color

--- women_safety_app/lib/utils/color.dart
+++ women_safety_app/lib/utils/color.dart
@@ -5,3 +5,6 @@ const Color rLightPink = Color(0xFFFFC8DD);
 const Color rDarkPink = Color(0xFFFFAFCC);
 const Color rLightBlue = Color(0xFFBDE0FE);
 const Color rDarkBlue = Color(0xFFA2D2FF);
+const Color rBottomBar = Color.fromARGB(255, 162, 255, 177);
+const Color rBottomNavigationBarItem = Color.fromARGB(255, 0, 173, 40);
+const Color rUnselectedItemColor = Color.fromARGB(255, 94, 151, 108);
