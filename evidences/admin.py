87ef61c972cee7acc46843206efd684dba52ab63
AnaidaLewis <anaidallewis@gmail.com>
2023-07-29 16:57:09 +0530

    registered models

--- evidences/admin.py
+++ evidences/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Evidence,EvidenceSuspect,Suspect
 # Register your models here.
+
+admin.site.register(Evidence)
+admin.site.register(EvidenceSuspect)
+admin.site.register(Suspect)
