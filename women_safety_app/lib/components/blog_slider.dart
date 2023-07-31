7fe7737eb72979191ac2bc7cbdabd1d2e3c461a3
Krish <krishshah235@gmail.com>
2023-07-31 14:15:51 +0530

    Slogans added

--- women_safety_app/lib/components/blog_slider.dart
+++ women_safety_app/lib/components/blog_slider.dart
@@ -3,33 +3,76 @@ import 'package:flutter/material.dart';
 
 class BlogSlider extends StatelessWidget {
   final List<String> imageList;
+  final List<String> sloganList; // List of text slogans
 
-  const BlogSlider({Key? key, required this.imageList});
+  const BlogSlider({
+    Key? key,
+    required this.imageList,
+    required this.sloganList,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return CarouselSlider.builder(
-      options: CarouselOptions(
-        autoPlay: true,
-        autoPlayInterval: const Duration(seconds: 1),
-        height: MediaQuery.of(context).size.height * 0.3,
-        initialPage: 0,
-        enableInfiniteScroll: true,
-        viewportFraction: 0.95,
-      ),
-      itemCount: imageList.length,
-      itemBuilder: (BuildContext context, int index, int realIndex) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 5),
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5),
+    return Column(
+      children: [
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.25,
+          width: MediaQuery.of(context).size.width * 0.9,
+          child: CarouselSlider.builder(
+            options: CarouselOptions(
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 4),
+              enableInfiniteScroll: true,
+              viewportFraction: 0.95,
             ),
-            child: Image.asset(imageList[index]),
+            itemCount: imageList.length,
+            itemBuilder: (BuildContext context, int index, int realIndex) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5, vertical: 5),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Image.asset(imageList[index]),
+                ),
+              );
+            },
           ),
-        );
-      },
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.1,
+          width: MediaQuery.of(context).size.width * 1,
+          child: CarouselSlider.builder(
+            options: CarouselOptions(
+              autoPlay: true,
+              autoPlayInterval: const Duration(seconds: 2),
+              enableInfiniteScroll: true,
+              viewportFraction: 0.9,
+            ),
+            itemCount: sloganList.length,
+            itemBuilder: (BuildContext context, int index, int realIndex) {
+              return Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Center(
+                    child: Text(
+                      sloganList[index],
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
--- women_safety_app/lib/screens/home_screen.dart
+++ women_safety_app/lib/screens/home_screen.dart
@@ -16,13 +16,31 @@ class _StartScreenState extends State<StartScreen> {
     'assets/images/temples.png',
     'assets/images/helpline.png',
   ];
+  final List<String> sloganList = [
+    'Women safety first!',
+    'Empowered women empower women!',
+    'Speak up, rise up!',
+    'Break the silence, end the violence!',
+    'You are strong, you are brave, you are unstoppable!',
+    'Safety is a basic human right!',
+    'Together we stand, united we protect!',
+    'No excuse for abuse!',
+    'Empowerment is the key to progress!',
+    'Respect, equality, and dignity for all!',
+    'Be the change you want to see in the world!',
+    'Support, uplift, and inspire!',
+    'Raise your voice, make a difference!',
+    'Safe women, strong nation!',
+    'Stand tall, speak up!',
+    'Safety is not negotiable!',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const AppBarConstant(),
-      backgroundColor: rPurple,
-      body: BlogSlider(imageList: imageList),
+      backgroundColor: rBackground,
+      body: BlogSlider(imageList: imageList, sloganList: sloganList),
     );
   }
 }
