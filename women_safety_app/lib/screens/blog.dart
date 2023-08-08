01e05d124c2d9dcb9e17c036b5b845eea4c92e97
Vidhi <vidhikkansara@gmail.com>
2023-08-08 12:58:46 +0530

    Blog Screen done!

--- women_safety_app/lib/screens/blog.dart
+++ women_safety_app/lib/screens/blog.dart
@@ -8,34 +8,21 @@ class Blog {
   String author;
   String imageURL;
 
-  Blog({required this.title, required this.author, required this.content, required this.imageURL});
+  Blog(
+      {required this.title,
+      required this.author,
+      required this.content,
+      required this.imageURL});
 }
 
-// class Blogdisplay {
-//   Future<List<Blog>> fetchBlogs() async {
-//     // final response = await http.get('');
-//
-//     if (response.statusCode == 200) {
-//       final data = json.decode(response.body);
-//       return List<Blog>.from(data.map((post) => Blog(
-//             title: post['title'],
-//             content: post['content'],
-//             author: post['author'],
-//           )));
-//     } else {
-//       throw Exception('Failed to load blog posts');
-//     }
-//   }
-// }
-
-// Future<List<Blog>> getBlogs(String authToken) async {
-  Future<List<Blog>> getBlogs() async {
+Future<List<Blog>> getBlogs() async {
   final response = await http.get(
     // Uri.parse('http://localhost:8000/blogs/1/'),
     Uri.parse('https://rakshika.onrender.com/blogs/1/'),
     headers: {
       'accept': 'application/json',
-      // 'Authorization': 'Token $authToken',
+      'Authorization':
+          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNjkxNzI4NDU0LCJpYXQiOjE2OTE0NjkyNTQsImp0aSI6ImY3MzYyMzc5Y2I5MTQ1MTM4OWM0MDU2M2ZlMjY3ODE4IiwidXNlcl9pZCI6NX0.LmamNhJ_rMYSSeygaB677gxVCjSBpsxUSjJU0XaHO9U',
     },
   );
 
@@ -57,20 +44,6 @@ class Blog {
   }
 }
 
-// final List<Blog> blogDisplay = [
-//   Blog(
-//     title: 'First Blog ',
-//     content: 'This is the content of the first blog ...',
-//     author: 'Vidhi K',
-//   ),
-//   Blog(
-//     title: 'Second Blog ',
-//     content: 'This is the content of the second blog ...',
-//     author: 'Vidhi K',
-//   ),
-// ];
-List<Blog> blogDisplay = [];
-
 class BlogScreen extends StatefulWidget {
   const BlogScreen({super.key});
 
@@ -79,6 +52,7 @@ class BlogScreen extends StatefulWidget {
 }
 
 class _BlogScreenState extends State<BlogScreen> {
+  List<Blog> blogDisplay = [];
   @override
   void initState() {
     super.initState();
@@ -89,6 +63,7 @@ class _BlogScreenState extends State<BlogScreen> {
       });
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -114,11 +89,27 @@ class _BlogScreenState extends State<BlogScreen> {
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  // Display the minimized image
+                  post.imageURL.isNotEmpty
+                      ? SizedBox(
+                          height: 150, // Set the desired height
+                          width: double
+                              .infinity, // Match the width of the container
+                          child: Image.network(
+                            post.imageURL,
+                            fit:
+                                BoxFit.cover, // Adjust the image's aspect ratio
+                          ),
+                        )
+                      : const SizedBox.shrink(),
+                  const SizedBox(height: 8.0),
                   Text(
                     post.title,
                     style: const TextStyle(
                         fontSize: 18.0, fontWeight: FontWeight.bold),
                   ),
+                  const SizedBox(height: 12.0),
+                  Text(post.content),
                   const SizedBox(height: 8.0),
                   Text('By ${post.author}'),
                 ],
