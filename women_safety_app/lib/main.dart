d5b3a0474363c9067d1e26403b418719df9ecc43
Vijay Harkare <vijay.harkare2020@gmail.com>
2023-08-16 20:55:36 +0530

    minor bug fixes

--- women_safety_app/lib/main.dart
+++ women_safety_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:async';
-import 'package:geolocator/geolocator.dart';
+// import 'package:geolocator/geolocator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:Rakshika/components/bottom_bar.dart';
@@ -7,11 +7,12 @@ import 'package:Rakshika/components/bottom_bar_admin.dart';
 import 'package:Rakshika/components/bottom_bar_male.dart';
 import 'package:record/record.dart';
 import 'package:audioplayers/audioplayers.dart';
-
+// import 'dart:convert';
 import 'package:Rakshika/screens/log_in.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:http/http.dart' as http;
+// import 'package:http/http.dart' as http;
 // import 'package:Rakshika/screens/register.dart';
+// import 'package:workmanager/workmanager.dart';
 
 final storage = const FlutterSecureStorage();
 
@@ -46,37 +47,73 @@ class _AppStartState extends State<AppStart> {
   String? GENDER;
   late Record audioRecord;
   late AudioPlayer audioPlayer;
+  // Timer? _timer;
+  // Position? _lastPosition;
+
+  // void _startSendingLocationUpdates() async {
+  //   ACCESS_LOGIN = await storage.read(key: 'access_login');
+  //   if (ACCESS_LOGIN != null) {
+  //     _listenToPosStream();
+  //   }
+  // }
+
+  // void _listenToPosStream() {
+  //   Geolocator.getPositionStream().listen((Position position) {
+  //     if (position != null) {
+  //       _lastPosition = position;
+  //       if (_timer == null) {
+  //         _sendLocationUpdates(ACCESS_LOGIN!, position.latitude.toString(), position.longitude.toString());
+  //         _startTimer();
+  //       }
+  //     }
+  //   });
+  // }
+
+  // void _startTimer() {
+  //   const interval = const Duration(seconds: 30);
+  //   _timer = Timer.periodic(interval, (Timer t) => _sendLocationUpdates(ACCESS_LOGIN!, _lastPosition!.latitude.toString(), _lastPosition!.longitude.toString()));
+  // }
+
+  // Future<bool> _sendLocationUpdates(String authToken, String latitude, String longitude) async {
+  //   // Get the user's current location
+  //   print("Getting location...");
+  //   // final Position position = await Geolocator.getCurrentPosition();
+  //   print("Latitude: ${latitude.toString()}, Longitude: ${longitude.toString()}");
+  //   print("Sending location to backend...");
+  //   const backendUrl = 'https://rakshika.onrender.com';
+  //   // Send the location update to the backend
+  //   final response = await http.post(
+  //     Uri.parse('$backendUrl/location_stream/update_location/'),
+  //     body: jsonEncode({
+  //       'latitude': latitude.toString(),
+  //       'longitude': longitude.toString(),
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json',
+  //       'Authorization': 'Bearer $authToken'
+  //     }
+  //   );
+
+  //   // Handle the response from the backend
+  //   if (response.statusCode == 200) {
+  //     // print(position.latitude.toString());
+  //     // print(position.longitude.toString());
+  //     print('Location update sent successfully');
+  //     return true;
+  //   } else {
+  //     print('Failed to send location update: ${response.reasonPhrase} ${response.body}');
+  //     // throw Exception('Failed to send location update');
+  //     return false;
+  //   }
+  // }
+
   @override
   void initState() {
     super.initState();
     _checkTokenAndGender();
+    // _startSendingLocationUpdates();
     // if (ACCESS_LOGIN != null)
     // sendLocationUpdates();
-  }
-
-  void sendLocationUpdates() async {
-    // Create a new repeating timer that runs every 15 minutes
-    Timer.periodic(const Duration(minutes: 15), (Timer timer) async {
-      // Get the user's current location
-      final Position position = await Geolocator.getCurrentPosition();
-      const backendUrl = 'https://rakshika.onrender.com/';
-      // Send the location update to the backend
-      final response = await http.post(
-        Uri.parse('$backendUrl/update-location'),
-        body: {
-          'latitude': position.latitude.toString(),
-          'longitude': position.longitude.toString(),
-        },
-      );
-
-      // Handle the response from the backend
-      if (response.statusCode == 200) {
-        print('Location update sent successfully');
-      } else {
-        print('Failed to send location update: ${response.body}');
-        throw Exception('Failed to send location update');
-      }
-    });
   }
 
   Future<void> _checkTokenAndGender() async {
